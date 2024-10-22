0767851d0148b05c32320ed3b7770d0e697c4dd3
andremlr <84641684+andremlr@users.noreply.github.com>
2024-10-22 13:54:44 -0300

    Update Pagamento.java

--- Domain/Pagamento.java
+++ Domain/Pagamento.java
@@ -1,13 +1,31 @@
 package Domain;
 
-public class Pagamento {
-    private String formaPagamento;
-    private Usuario paga;
-    private Usuario recebe;
-    private double valor;
-    private int idUsuarioPagador;
-    private int idUsuarioPrestador;
-    private int idServico;
+import javax.persistence.*;
 
-    public void efetuarPagamento(){}
+@Entity
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String formaPagamento;
+    private double valor;
+    
+    @ManyToOne
+    @JoinColumn(name = "servico_id")
+    private Servico servico;
+    
+    @ManyToOne
+    @JoinColumn(name = "usuario_pagador_id")
+    private Usuario usuarioPagador;
+    
+    @ManyToOne
+    @JoinColumn(name = "usuario_prestador_id")
+    private Usuario usuarioPrestador;
+
+    // Getters and Setters
+    
+    public void efetuarPagamento() {
+        // lógica de pagamento
+    }
 }
